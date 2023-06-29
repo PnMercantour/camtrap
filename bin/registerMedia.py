85a308ca995038f2515ab378c542f606912c1014
Vincent Prunet <vincent.prunet@mercantour-parcnational.fr>
2023-06-29 15:50:50 +0200

    media files in db

--- bin/registerMedia.py
+++ bin/registerMedia.py
@@ -18,67 +18,76 @@ POSTGRES_CONNECTION = getenv("POSTGRES_CONNECTION")
 
 
 def pg_new_project(args):
-    with psycopg.connect(args.pg) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(
-                """
+    with args.conn.cursor() as cursor:
+        cursor.execute(
+            """
 insert into camtrap.project(name, creation_date)
 values(%s, %s)
 on conflict (name) do update
 set update_date = excluded.creation_date
 returning id
 """,
-                (args.project, datetime.now()),
-            )
-            (id,) = cursor.fetchone()
-            return id
+            (args.project, datetime.now()),
+        )
+        (id,) = cursor.fetchone()
+        return id
 
 
-def pg_new_file(path, level, parent_id, args):
-    with psycopg.connect(args.pg) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(
-                """
+def pg_new_file(path, parent_id, args):
+    level = len(path.parts)
+    with args.conn.cursor() as cursor:
+        cursor.execute(
+            """
 insert into camtrap.file(path, project_id, parent_id, level, creation_date)
 values(%s, %s, %s, %s, %s)
 on conflict (project_id, path) do update
 set (parent_id, level, update_date) = (excluded.parent_id, excluded.level, excluded.creation_date)
 returning id
 """,
-                (str(path), args.project_id, parent_id, level, datetime.now()),
-            )
-            (id,) = cursor.fetchone()
-            return id
+            (str(path), args.project_id, parent_id, level, datetime.now()),
+        )
+        (id,) = cursor.fetchone()
+        return id
 
 
-def pg_insert_media(exif, relative_path, level, parent_id, args):
-    with psycopg.connect(args.pg) as conn:
-        with conn.cursor() as cursor:
-            for record in exif:
-                media_path = record["SourceFile"]
-                file_type = record["File:FileType"]
-                mime_type = record["File:MIMEType"]
-                if file_type == "MP4":
-                    start_time = (
-                        datetime.strptime(
-                            record["QuickTime:CreateDate"], exiftoolDateFormat
-                        )
-                    ).isoformat()
-                    duration = record["QuickTime:Duration"]
-                elif file_type == "JPEG":
-                    start_time = (
-                        datetime.strptime(
-                            record["EXIF:DateTimeOriginal"], exiftoolDateFormat
-                        )
-                    ).isoformat()
-                    duration = 0
-                else:
-                    print(media_path, "ignored: unhandled filetype", file_type)
-                    continue
-                id = pg_new_file(media_path, level, parent_id, args)
+def pg_file_chain(path, args):
+    """
+    ensure that path parents are all created in pg up to root
+    Return <id> of parent file"""
+    if len(path.parts) == 0:
+        return None
+    else:
+        grand_parent_id = pg_file_chain(path.parent, args)
+        return pg_new_file(path.parent, grand_parent_id, args)
 
-                cursor.execute(
-                    """
+
+def pg_insert_media(exif, parent_id, args):
+    with args.conn.cursor() as cursor:
+        for record in exif:
+            media_path = Path(record["SourceFile"])
+            file_type = record["File:FileType"]
+            mime_type = record["File:MIMEType"]
+            if file_type == "MP4":
+                start_time = (
+                    datetime.strptime(
+                        record["QuickTime:CreateDate"], exiftoolDateFormat
+                    )
+                ).isoformat()
+                duration = record["QuickTime:Duration"]
+            elif file_type == "JPEG":
+                start_time = (
+                    datetime.strptime(
+                        record["EXIF:DateTimeOriginal"], exiftoolDateFormat
+                    )
+                ).isoformat()
+                duration = 0
+            else:
+                print(media_path, "ignored: unhandled filetype", file_type)
+                continue
+            id = pg_new_file(media_path, parent_id, args)
+
+            cursor.execute(
+                """
 insert into camtrap.media(
     project_id, id, file_type, mime_type, start_time, duration, creation_date
     ) 
@@ -87,20 +96,20 @@ on conflict(id) do update
 set (file_type, mime_type, start_time, duration, update_date) = 
 (excluded.file_type, excluded.mime_type, excluded.start_time, excluded.duration, excluded.creation_date)
 """,
-                    (
-                        args.project_id,
-                        id,
-                        file_type,
-                        mime_type,
-                        start_time,
-                        duration,
-                        datetime.now(),
-                    ),
-                )
+                (
+                    args.project_id,
+                    id,
+                    file_type,
+                    mime_type,
+                    start_time,
+                    duration,
+                    datetime.now(),
+                ),
+            )
 
 
-def processPath(relative_path, level, parent_id, args):
-    print("Entering", relative_path, level, parent_id)
+def processPath(relative_path, parent_id, args):
+    print("Entering", relative_path, parent_id)
     path = args.root / relative_path
     if not path.exists():
         print(f"relative path {relative_path} does not exist")
@@ -110,11 +119,11 @@ def processPath(relative_path, level, parent_id, args):
         return
     else:
         (args.output / relative_path).mkdir(parents=True, exist_ok=True)
-        id = pg_new_file(relative_path, level, parent_id, args)
+        id = pg_new_file(relative_path, parent_id, args)
         for p in path.iterdir():
             rel = p.relative_to(args.root)
             if p.is_dir():
-                processPath(rel, level + 1, id, args)
+                processPath(rel, id, args)
         try:
             # https://exiftool.org/exiftool_pod.html
             print("Processing", relative_path)
@@ -135,14 +144,15 @@ def processPath(relative_path, level, parent_id, args):
             )
             sub.check_returncode()
             if len(sub.stdout) > 0:
-                result = json.loads(sub.stdout)
+                exif = json.loads(sub.stdout)
                 if args.insert:
-                    pg_insert_media(result, relative_path, level + 1, id, args)
+                    pg_insert_media(exif, id, args)
                 with (args.output / (relative_path) / "exif.json").open("w") as f:
-                    json.dump(result, f)
+                    json.dump(exif, f)
         except Exception as e:
             print(e)
             print("ERROR: processPath", relative_path)
+            raise
 
 
 if __name__ == "__main__":
@@ -201,7 +211,10 @@ if __name__ == "__main__":
     else:
         args.files = [p.resolve(strict=True) for p in args.files]
     args.output.mkdir(parents=True, exist_ok=True)
-
-    args.project_id = pg_new_project(args)
-    for path in args.files:
-        processPath(path.relative_to(args.root), 0, None, args)
+    with psycopg.connect(args.pg) as conn:
+        args.conn = conn
+        args.project_id = pg_new_project(args)
+        for path in args.files:
+            relative_path = path.relative_to(args.root)
+            parent_id = pg_file_chain(relative_path, args)
+            processPath(relative_path, parent_id, args)
